2e95b0e4271f6a226e0c0d4b281995fc7cafbb0b
chenji336 <1010072738@qq.com>
2018-04-02 17:53:26 +0800

    todo

--- 从0开始搭建vue+webpack/src/app.vue
+++ 从0开始搭建vue+webpack/src/app.vue
@@ -1,19 +1,44 @@
 ﻿<template>
-	<div id='test'>{{text}}</div>
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
+    <todo></todo>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				text: 'abcde'
-			}
-		}
-	}
+import Header from './todo/header.vue'
+import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Todo,
+  }
+}
 </script>
 
-<style type="text/css">
-	#test {
-		color: red;
-	}
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
 </style>
+
+
--- 从0开始搭建vue+webpack/src/index.js
+++ 从0开始搭建vue+webpack/src/index.js
@@ -1,9 +1,12 @@
 ﻿import Vue from 'vue'
 import App from './app.vue'
 
-import './assets/images/bg.jpeg'
+// 最开始测试使用的，为了验证url-loader以及css-loader、stylus-loader
+/*import './assets/images/bg.jpeg'
 import './assets/styles/test.css'
-import './assets/styles/test-stylus.styl'
+import './assets/styles/test-stylus.styl'*/
+
+import './assets/styles/global.styl'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
